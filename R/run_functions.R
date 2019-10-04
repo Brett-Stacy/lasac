c127953f4d772c0713d5d192df0efa57ee9e4f0b
Brett Stacy <45962770+Brett-Stacy@users.noreply.github.com>
2019-10-04 14:41:48 +1000

    first script

---
+++ R/run_functions.R
@@ -0,0 +1,17 @@
+# Functions
+
+
+
+
+
+#' Plot Output
+#'
+#' Some plotting function that displays output from an OM/EM run using lasac
+#'
+#' Further details go here
+#'
+#' @param output
+#' @export
+myplot = function(output){
+  plot(output)
+}
