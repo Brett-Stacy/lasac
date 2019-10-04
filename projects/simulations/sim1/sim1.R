4cbb668ea4391d61c5bf5b9b16987241e7085754
Brett Stacy <45962770+Brett-Stacy@users.noreply.github.com>
2019-10-04 15:46:55 +1000

    add sim tests

---
+++ projects/simulations/sim1/sim1.R
@@ -0,0 +1,24 @@
+# Try simulating OM data with CASAL
+# 4/10/19
+
+
+
+
+## Packages ----
+library(casal)
+library(lasac)
+
+## House ----
+house = "C:/Users/STA384/Documents/GitHub/lasac"
+setwd(house)
+
+
+
+# re-read power points
+# read in the simulated observations into R
+# save/plot ssb
+# paste the observations into the estimating model EMestimation.csl and EMpopulation.csl?
+# run casal -e
+# compare ssb true to ssb est. should be the same or similar.
+# do it again but change catch in EMpopulation.csl to say half.
+# compare ssb again.
