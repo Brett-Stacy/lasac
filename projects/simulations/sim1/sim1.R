28c41f03fbe975b6adef06cd8f226bd23356d4a2
Brett Stacy <45962770+Brett-Stacy@users.noreply.github.com>
2019-10-08 08:28:53 +1100

    update

--- projects/simulations/sim1/sim1.R
+++ projects/simulations/sim1/sim1.R
@@ -2,6 +2,19 @@
 # 4/10/19
 
 
+# Generate a free parameter file (e.g. the MPD)
+# casal -e -q -O true.dat -f casal_
+
+# Simulate 5 (or in a real experiment, many more) observations
+# casal -s 5 tempSIM -i true.dat
+
+# run casal on simulated
+# casal -e -q -f EM > EMoutput.log
+
+
+# The point is to get a baseline with correct simulated observations from correct catch. Then estimate it for a baseline. Check this baseline SSB against the original csl file -e. Just do that one time, but simulate 1000 times
+# Then simulate data when there is IUU catch, then estimate it using the observations, but still with the original assumed catch. Extract SSB and check it against the baseline
+
 
 
 ## Packages ----
@@ -9,13 +22,22 @@ library(casal)
 library(lasac)
 
 ## House ----
-house = "C:/Users/STA384/Documents/GitHub/lasac"
+house = paste0("C:/Users/", Sys.info()["login"], "/Documents/GitHub/lasac")
 setwd(house)
 
 
 
 # re-read power points
-# read in the simulated observations into R
+# figure out how to run simulations from R, then read them in, then paste them into est.csl, then run casal, then extract SSB
+## read in the simulated observations into R
+casalest = casal::extract.csl.file(paste0("C:/Users/", Sys.info()["login"], "/Documents/GitHub/lasac/projects/simulations/sim1/","tempSIM.par1.sim1"))
+
+casalest2 = casal::extract.csl.file(paste0("C:/Users/", Sys.info()["login"], "/Documents/GitHub/lasac/projects/simulations/sim1/","casal_estimation_skel.csl"))
+
+casalest3 = append(casalest2, casalest)
+
+casal::write.csl.file(casalest3, paste0("C:/Users/", Sys.info()["login"], "/Documents/GitHub/lasac/projects/simulations/sim1/","EMestimation.csl"))
+
 # save/plot ssb
 # paste the observations into the estimating model EMestimation.csl and EMpopulation.csl?
 # run casal -e
